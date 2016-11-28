aa3e7df20b05882e315f583a7f4d56c1d75d8fd6
Ben Zimmerman <benjamin.zimmerman@gmail.com>
2016-11-27 19:22:33 -0500

    add requirement #2 and #4

--- farmlogger/farm/views.py
+++ farmlogger/farm/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+import pytz
+from django.utils.dateparse import parse_datetime
 
 from farmlogger.farm.models import Event
 from farmlogger.farm.serializers import EventSerializer
@@ -6,4 +8,23 @@ from farmlogger.farm.serializers import EventSerializer
 
 class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
-    queryset = Event.objects.all()
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned events by filtering against query
+        parameters in the URL.
+        """
+        queryset = Event.objects.all()
+        user_id = self.request.query_params.get('user_id', None)
+        if user_id:
+            user_id = int(user_id)
+            queryset = queryset.filter(user__id=user_id)
+        timestamp_gt = self.request.query_params.get('timestamp_gt', None)
+        if timestamp_gt:
+            timestamp_gt = pytz.timezone('UTC').localize(parse_datetime(timestamp_gt))
+            queryset = queryset.filter(timestamp__gt=timestamp_gt)
+        timestamp_lt = self.request.query_params.get('timestamp_lt', None)
+        if timestamp_lt:
+            timestamp_lt = pytz.timezone('UTC').localize(parse_datetime(timestamp_lt))
+            queryset = queryset.filter(timestamp__lt=timestamp_lt)
+        return queryset
--- utility/test_api.py
+++ utility/test_api.py
@@ -6,18 +6,32 @@ SERVER_URL = 'http://192.168.83.20:8000/apis/'
 EVENT_URL = SERVER_URL + 'events/'
 
 
-def test_api():
+def ingest_events():
+    print('Requirement #1 Ingest events')
     error_count = 0
     for x, line in enumerate(open('sample_events.txt').readlines()):
         event = json.loads(line)
         r = requests.post(EVENT_URL, json=event,)
         if r.status_code > 299:
             error_count += 1
-            print('line: {} code: {} error: {}'.format(x, r.status_code, r.text))
+            print('line: {} code: {} error: {}'.format(x + 1, r.status_code, r.text))
     print('{} errors found'.format(error_count))
 
-    print('\nRequirement #1')
-    for event in requests.get(EVENT_URL, json=event,)
+
+def test_api():
+    ingest_events()
+
+    print('\nRequirement #2')
+    print('All events created by user 162\n')
+    for event in requests.get(EVENT_URL + '?user_id=162').json():
+        print(event)
+
+    print('\nRequirement #4')
+    print('All events after 2016-11-01T16:56:55.436642 and before 2016-11-01T16:56:56.437836.\n')
+# after 346 and before 357
+    args = '?timestamp_gt=2016-11-01T16:56:55.436642&timestamp_lt=2016-11-01T16:56:56.437836'
+    for event in requests.get(EVENT_URL + args).json():
+        print(event)
 
 
 if __name__ == '__main__':
